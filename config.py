e374924162f98c645a02e0eb3c84e8931d4f0e3f
sadmoondog <oleg.minaev@gmail.com>
2020-09-04 18:09:17 +0300

    Added config

---
+++ config.py
@@ -0,0 +1,12 @@
+API_KEY = ''
+QRADAR_URL = ''
+QRADAR_TOKEN = ''
+INTERVAL = 3600
+CHUNK_SIZE = 10000
+LOAD_BULK = True
+FS_TI_URL = ''
+FEEDS = {
+    'fs_ti': 'ti.json',
+    'fs_ti_criminal': 'ti.json?criminal=true',
+    'fs_ti_criminal_wo_cdn': 'ti.json?criminal=true&cdn=false'
+}
